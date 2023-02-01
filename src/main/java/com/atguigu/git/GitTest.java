37628aeecb025a16db57dac1923b0096edd9d532
ly <1720123258@qq.com>
2023-02-01 20:55:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @Description: TODO
+ * @BelongsProject: git-test
+ * @BelongsPackage: com.atguigu.git
+ * @Author: liuyu
+ * @CreateTime: 2023-02-01  20:50
+ * @Version: 1.0
+ */
+public class GitTest {
+
+    public static void main(String args[]){
+        System.out.println("hello git !!!");
+    }
+}
